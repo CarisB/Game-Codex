8308fc0603000e994c96dfeed0ce8b828fb16a4a
CarisB <chkbaek@gmail.com>
2024-05-29 13:03:14 -0500

    Added loading GameCardSkeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,22 +24,29 @@ interface GameList {
 function useGames() {
   const [gameList, setGameList] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
+
     apiClient
       .get<GameList>("/games", { signal: controller.signal })
-      .then((res) => setGameList(res.data.results))
+      .then((res) => {
+        setGameList(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { gameList, error };
+  return { gameList, error, isLoading };
 }
 
 export default useGames;
