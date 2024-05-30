c0e36d8f449dac79717295cd53064272d5eaa3ea
CarisB <chkbaek@gmail.com>
2024-05-29 22:19:54 -0500

    Built SearchBar

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,16 +1,18 @@
 import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 
 function ColorModeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <HStack>
+      <BsFillSunFill />
       <Switch
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
         colorScheme="green"
       />
-      <Text>Dark Mode</Text>
+      <BsFillMoonFill />
     </HStack>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchBar from "./SearchBar";
 
 function NavBar() {
   return (
-    <HStack justifyContent="space-between">
+    <HStack spacing={10}>
       <Image src={logo} boxSize="60px" />
+      <SearchBar />
       <ColorModeSwitch />
     </HStack>
   );
