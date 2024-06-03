5e1ce82930b5fd85b508c85651770242a6f267aa
CarisB <chkbaek@gmail.com>
2024-06-03 09:33:31 -0500

    Removed unused file

--- src/hooks/useData.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { AxiosRequestConfig, CanceledError } from "axios";
-
-export interface DataList<T> {
-  count: number;
-  results: T[];
-}
