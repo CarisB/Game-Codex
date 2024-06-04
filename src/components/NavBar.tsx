e7289aff7829395efb3a205e18a7c3f042a85aa1
CarisB <chkbaek@gmail.com>
2024-06-03 19:08:52 -0500

    Reset gameQuery when sending back to HomePage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,15 @@ import logo from "../assets/logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBar from "./SearchBar";
 import { Link } from "react-router-dom";
+import useGameQueryStore from "../gameQueryStore";
 
 function NavBar() {
   const [isMobile] = useMediaQuery("(max-width: 46em)");
 
   return (
     <HStack spacing={10}>
-      <Link to="/">
+      {/* Resets gameQuery before sending user back to HomePage */}
+      <Link onClick={() => (useGameQueryStore().gameQuery = {})} to="/">
         <Image src={logo} boxSize="60px" />
       </Link>
       <SearchBar />
